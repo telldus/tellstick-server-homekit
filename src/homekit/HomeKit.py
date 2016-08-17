8f3e2ad38aa5d2201c8775d10d346a7ef923587e
Micke Prag <micke.prag@telldus.se>
2016-08-17 09:36:15 +0200

    Implement call to /accessories

--- src/homekit/HomeKit.py
+++ src/homekit/HomeKit.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from base import Application, Plugin, Settings
+from base import Application, Plugin, Settings, implements
+from board import Board
+from telldus import DeviceManager, IDeviceChange, Device
 from Bonjour import Bonjour
 from HapHandler import HapHandler
 from SocketServer import TCPServer
 from threading import Thread
+from urlparse import urlparse, parse_qsl
+
+from HapAccessory import HapAccessory, HapCharacteristic, HapService
 import random
 import ed25519
 
 import logging
 import json
-from board import Board
+
 
 class RequestHandler(HapHandler):
 	def finish(self):
@@ -19,6 +24,10 @@ class RequestHandler(HapHandler):
 
 	def do_encrypted_GET(self):
 		logging.warning('Encrypted GET to %s', self.path)
+		hk = HomeKit(RequestHandler.HTTPDServer.context)
+		url = urlparse(self.path)
+		if url.path == '/accessories':
+			hk.handleAccessories(self)
 
 	def setup(self):
 		HapHandler.setup(self)
@@ -50,14 +59,44 @@ class HTTPDServer(object):
 		#self.httpServer.shutdown()
 		logging.warning("Server was shut down")
 
+class HapDeviceStateCharacteristics(HapCharacteristic):
+	def __init__(self):
+		super(HapDeviceStateCharacteristics,self).__init__(value=self.value(), format='bool', type='25', perms=['pr', 'pw', 'ev'])
+
+class HapBridgeAccessory(HapAccessory):
+	def __init__(self):
+		super(HapBridgeAccessory,self).__init__('Telldus Technologies', Board.product(), 'Mickes dator', HapHandler.getId())
+
+class HapDeviceAccessory(HapAccessory):
+	def __init__(self, device):
+		super(HapDeviceAccessory,self).__init__('Acme', device.typeString(), device.name(), device.id())
+		self.device = device
+		service = HapService('43')
+		service.addCharacteristics(HapCharacteristic(device.name(), type='23', perms=['pr']))
+		service.addCharacteristics(HapDeviceStateCharacteristics())
+		self.addService(service)
+
 class HomeKit(Plugin):
+	implements(IDeviceChange)
+
 	def __init__(self):
 		Application().queue(self.start)
+		self.accessories = {}
+		self.accessories[1] = HapBridgeAccessory()
 
 	def start(self):
 		self.port    = random.randint(8000, 8080)
 		self.bonjour = Bonjour(port=self.port)
 		self.httpServer = HTTPDServer(port=self.port, context=self.context)
+		deviceManager = DeviceManager(self.context)
+		for device in deviceManager.retrieveDevices():
+			if not device.confirmed():
+				continue
+			self.deviceAdded(device)
+
+	def handleAccessories(self, request):
+		accessories = [{'aid': i, 'services': self.accessories[i].servicesJSON()} for i in self.accessories]
+		request.sendEncryptedResponse({'accessories': accessories})
 
 	def newConnection(self, conn):
 		s = Settings('homekit')
@@ -73,4 +112,13 @@ class HomeKit(Plugin):
 			s['password'] = password
 		conn.setLongTermKey(longTermKey, password)
 
+	# IDeviceChange
+	def deviceAdded(self, device):
+		if device.isDevice() == False:
+			return
+		i = max(self.accessories)+1
+		self.accessories[i] = HapDeviceAccessory(device)
 
+	# IDeviceChange
+	def deviceConfirmed(self, device):
+		self.deviceAdded(device)
